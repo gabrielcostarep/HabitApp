0fdf3b7051c2d1d17c228c9bb568b8a9f497643d
gabrielcostarep <gabrielcostadev@hotmail.com>
2024-02-20 02:37:39 -0300

    SignInView Validate Form

--- Habit/SignIn/ViewModel/SignInViewStates.swift
+++ Habit/SignIn/ViewModel/SignInViewStates.swift
@@ -1,15 +1,8 @@
 //
-//  SignInViewFormStates.swift
+//  SignInViewStates.swift
 //  Habit
 //
 //  Created by Gabriel Costa on 20/02/24.
 //
 
-import SwiftUI
-
-struct SignInViewFormStates {
-	@State var email     = ""
-	@State var password  = ""
-	
-	@State var hiddenNavigation = true
-}
+import Foundation
