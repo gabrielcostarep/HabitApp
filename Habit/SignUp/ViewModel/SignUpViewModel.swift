c22f6c65be5e3eca559688be207eae158af7c83c
gabrielcostarep <gabrielcostadev@hotmail.com>
2024-03-02 04:55:36 -0300

    using the WebServices

--- Habit/SignUp/ViewModel/SignUpViewModel.swift
+++ Habit/SignUp/ViewModel/SignUpViewModel.swift
@@ -15,11 +15,21 @@ class SignUpViewModel: ObservableObject {
 	var publisher: PassthroughSubject<Bool, Never>!
 
 	func signUp() {
-		self.uiState = .loading
+		uiState = .loading
 
-		DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-			self.publisher.send(true)
-			self.uiState = .success
-		}
+		WebServices.postUser(
+			fullName: form.fullName,
+			email: form.email,
+			password: form.password,
+			cpf: form.cpf,
+			phone: form.phone,
+			birthday: form.birthday.split(separator: "/").reversed().joined(separator: "-"),
+			gender: form.gender.index
+		)
+
+//		DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//			self.publisher.send(true)
+//			self.uiState = .success
+//		}
 	}
 }
