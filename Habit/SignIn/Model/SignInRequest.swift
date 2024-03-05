ae03609ca8cbc8d4741e343a0de9b13213e34483
gabrielcostarep <gabrielcostadev@hotmail.com>
2024-03-04 22:53:48 -0300

    create a SignInRequest Model

---
+++ Habit/SignIn/Model/SignInRequest.swift
@@ -0,0 +1,18 @@
+//
+//  SignInRequest.swift
+//  Habit
+//
+//  Created by Gabriel Costa on 04/03/24.
+//
+
+import SwiftUI
+
+struct SignInRequest {
+	let email: String
+	let password: String
+	
+	enum CodingKeys: String, CodingKey {
+		case email = "username"
+		case password
+	}
+}
